a77564e5ffab48c8e3476709d6d5871cb85ad370
harupy <hkawamura0130@gmail.com>
2019-03-13 10:19:15 +0900

    add snippet feature

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,4 +1,3 @@
 const s = document.createElement('script');
-s.setAttribute('type', 'module');
 s.src = chrome.extension.getURL('js/main.js');
 (document.head || document.documentElement).appendChild(s);
--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-import {enableSnippet} from './enableSnippet';
 
 (() => {
   const enableVim = cell => {
@@ -7,7 +6,7 @@ import {enableSnippet} from './enableSnippet';
     cell.CodeMirror.options.showCursorWhenSelecting = 'vim';
   };
 
-  const updateBackspace = cell => {
+  const enableBackspaceUnindent = cell => {
     const unindentOrBackspace = cm => {
       const {line, ch} = cm.getCursor();
       const cursorLine = cm.getLine(line);
@@ -24,40 +23,73 @@ import {enableSnippet} from './enableSnippet';
     cell.CodeMirror.options.extraKeys['Backspace'] = unindentOrBackspace;
   }
 
+  const enableSnippet = (cell) => {
+    const tabDefaultFunc = cell.CodeMirror.options.extraKeys['Tab'];
+    const expandSnippetOrIndent = cm => {
+      const cursor = cm.getCursor();
+      const cursorLine= cm.getLine(cursor.line);
+      const cursorLeft = cursorLine.slice(0, cursor.ch);
+      const regex = /[^a-zA-Z0-9_]?([a-zA-Z0-9_]+)$/;
+      const match = cursorLeft.match(regex)
+      if (!match) {
+        tabDefaultFunc(cm);
+        return
+      }
+      const prefix = match[1];
+      const head = {line: cursor.line, ch: cursor.ch - prefix.length};
+    
+      const snippets = {
+        'inp'     : 'import numpy as np',
+        'iplt'    : 'import matplotlib.pyplot as plt',
+        'ipd'     : 'import pandas as pd',
+        'pdrc'    : 'pd.read_csv()',
+      };
+    
+      if (prefix in snippets) {
+        const body = snippets[prefix];
+        cm.replaceRange(body, head, cursor);
+        const match = body.match(/\)+$/);
+        if (match) {
+          cm.moveH(-match[0].length, 'char');
+        }
+      } else {
+        tabDefaultFunc(cm);
+      }
+    }
+    cell.CodeMirror.options.extraKeys['Tab'] = expandSnippetOrIndent;
+  }
+
+  const updateCell = cell => {
+    enableVim(cell);
+    enableBackspaceUnindent(cell);
+    enableSnippet(cell);
+  }
+
   const onKeyUp = () => {
     const cellEditing = document.querySelector('div.CodeMirror-focused');
-    console.log(cellEditing);
     if (cellEditing && !cellEditing.CodeMirror.getOption('vimMode')) {
-      enableVim(cellEditing);
-      updateBackspace(cellEditing);
-      // enableSnippet(cellEditing);
+      updateCell(cellEditing);
     }
   };
   
   const updateExistingCells = () => {
     document.querySelectorAll('div.CodeMirror').forEach(cell => {
-      enableVim(cell);
-      updateBackspace(cell);
-      // enableSnippet(cellEditing);
+      updateCell(cell);
     })
   }
   
   setTimeout(updateExistingCells, 1000);
   document.addEventListener('keyup', onKeyUp);
 
-  // insert mode
+  // Vim keybindings
   CodeMirror.Vim.map('jk', '<Esc>', 'insert');
   CodeMirror.Vim.map("jk", "<Esc>", "insert");
-
-  // visual mode
   CodeMirror.Vim.map("J", "G", "visual")
   CodeMirror.Vim.map("K", "gg", "visual")
   CodeMirror.Vim.map("H", "^", "visual")
   CodeMirror.Vim.map("L", "$", "visual")
   CodeMirror.Vim.map("j", "gj", "visual")
   CodeMirror.Vim.map("k", "gk", "visual")
-
-  // normal mode
   CodeMirror.Vim.map("J", "G", "normal")
   CodeMirror.Vim.map("H", "^", "normal")
   CodeMirror.Vim.map("L", "$", "normal")
@@ -65,4 +97,3 @@ import {enableSnippet} from './enableSnippet';
   CodeMirror.Vim.map("k", "gk", "normal")
   CodeMirror.Vim.map("K", "gg", "normal")
 })();
-
